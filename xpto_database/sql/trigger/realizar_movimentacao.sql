efbbbbacdcce617b7064bb408c6e57c115075acf
Sergio Lisan <sergiolisan@gmail.com>
2021-04-01 02:03:24 -0300

    melhor forma

--- xpto_database/sql/trigger/realizar_movimentacao.sql
+++ xpto_database/sql/trigger/realizar_movimentacao.sql
@@ -10,17 +10,10 @@ DECLARE
    saldo_origem NUMBER (10, 2);
    saldo_destino NUMBER (10, 2);
 BEGIN 
-   DBMS_OUTPUT.PUT_LINE(:NEW.id);
    -- faz a movimentacao entre as contas bancarias, deduz da origem e deposita no destino
    -- e ja cobra a taxa de movimentacoes
-   SELECT saldo INTO saldo_origem FROM conta_bancaria WHERE id = :NEW.conta_origem_id;
-   SELECT saldo INTO saldo_destino FROM conta_bancaria WHERE id = :NEW.conta_destino_id;
-
-   saldo_origem := saldo_origem - (:NEW.valor + taxa);
-   saldo_destino := saldo_destino + :NEW.valor;
-
-   UPDATE conta_bancaria c SET c.saldo = saldo_origem WHERE c.id = :NEW.conta_origem_id;
-   UPDATE conta_bancaria c SET c.saldo = saldo_destino WHERE c.id = :NEW.conta_destino_id;
+   UPDATE conta_bancaria SET saldo = saldo - (:NEW.valor + taxa) WHERE id = :NEW.conta_origem_id;
+   UPDATE conta_bancaria SET saldo = saldo + :NEW.valor WHERE id = :NEW.conta_destino_id;
 
    -- faz o registro da cobranca
    INSERT INTO taxa (cobrado_id, valor, cobrada_em) 
